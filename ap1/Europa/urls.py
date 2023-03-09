23b560bb384967c7edca51aaf530a02ba266dc8f
Tais8675 <nascimentotaais@gmail.com>
2023-03-09 14:20:42 -0300

    terceiro commit

--- ap1/Europa/urls.py
+++ ap1/Europa/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.europa, name="europa"),
-    path("", views.frança, name="frança"),
-    path("", views.italia, name="italia"),
-    path("", views.suecia, name="suecia")
+    path("frança", views.frança, name="frança"),
+    path("italia", views.italia, name="italia"),
+    path("suecia", views.suecia, name="suecia")
 
 ]
